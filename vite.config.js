23563c65b4d111357a8775a77e96e3ae1fa8fbb4
Nickyzj628 <1927139831@qq.com>
2024-11-09 20:34:20 +0800

    feat: 配置路径别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import path from "path";
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
 import pagesPlugin from "vite-plugin-pages";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [solid(), pagesPlugin()],
 })
