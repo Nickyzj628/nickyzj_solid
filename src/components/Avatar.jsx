8d3d667bd90dbb20c1caa2ed5b8dc3d932acfb2b
Nickyzj628 <1927139831@qq.com>
2024-11-09 22:05:40 +0800

    feat: 完成 Avatar 用户头像组件

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,24 @@
+import { clsx } from "@/services/dom";
+import { getImage } from "@/services/network";
+import { createSignal } from "solid-js";
+
+/**
+ * 头像，默认显示当前用户的
+ * @param {Object} props
+ * @param {string} props.name 用户名，以此获取头像
+ * @param {string} props.className
+ */
+const Avatar = (props) => {
+  const [getSrc, setSrc] = createSignal(getImage(`Avatars/${props.name ?? "Guest"}.webp`));
+
+  return (
+    <img
+      src={getSrc()}
+      alt=""
+      className={clsx("size-12 rounded-full", props.className)}
+      onError={() => setSrc(getImage("Avatars/Guest.webp"))}
+    />
+  );
+};
+
+export default Avatar;
